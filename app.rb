4967afd033aba8805aaff67afc57530704018347
Norman Köhring <me@nkoehring.de>
2011-07-26 04:40:35 +0200

    statistics

--- app.rb
+++ app.rb
@@ -11,19 +11,43 @@ end
 
 get "/" do
   @db = IO.readlines('db')
+  @stats = begin
+             Marshal.load(File.open("stats", "r"))
+           rescue EOFError, Errno::ENOENT
+             {}
+           end
+
   haml :index
 end
 
 get "/:code" do
   # aw damn… there is no real point to optimise this
+  code = params[:code]
   idx = params[:code].to_i(36)
   db = IO.readlines('db')
   @url = db[idx].strip
+  return status 404 unless @url
 
-  if @url
-    haml :url
-  else
-    status 404
+  File.open("stats", File::RDWR|File::CREAT, 0644) do |f|
+    f.flock(File::LOCK_EX)
+
+    if f.size == 0
+      stats = { code.to_sym => 1 }
+    else
+      stats = Marshal.load(f)
+      f.rewind
+      if stats.include? code.to_sym
+        stats[code.to_sym] += 1
+      else
+        stats[code.to_sym] = 1
+      end
+    end
+
+    Marshal.dump(stats, f)
+    f.flush
+    f.flock(File::LOCK_UN)
   end
+
+  haml :url
 end
 
